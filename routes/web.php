0072ce31c2c9ed9954164f33fd827957136a6ab0
MD. Nahid Chowdhury <solveplex@yahoo.com>
2017-10-04 21:33:15 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route:: get('/', 'WelcomeController@index');
+
+Route:: get('/support', 'WelcomeController@support');
+
+Route:: get('/about', 'WelcomeController@about');
+
+Route:: get('/blog', 'WelcomeController@blog');
+
+Route:: get('/contact', 'WelcomeController@contact');
+
+
+
+
+
+
+
+
+
